0d162c3cafb623741b02959c5707c20f1f674085
IFFranciscoME <if.francisco.me@gmail.com>
2019-10-16 16:47:01 -0500

    procesos de obtencion de datos

--- anova/datos.py
+++ anova/datos.py
@@ -7,9 +7,11 @@
 
 import pandas as pd
 import numpy as np
+from datetime import timedelta
 
 from oandapyV20 import API
 import oandapyV20.endpoints.instruments as instruments
+pd.options.mode.chained_assignment = None                 # para evitar el warning enfadoso de indexacion
 
 # -- --------------------------------------------------------------------------------- Datos: Calendario Economico -- #
 # -- --------------------------------------------------------------------------------------- --------------------- -- #
@@ -20,8 +22,19 @@ df_ce = pd.read_csv(filepath_or_buffer='archivos/calendario_economico.csv')
 indicador = 'ISM Manufacturing PMI'
 df_ice = df_ce.iloc[np.where(df_ce['Name'] == indicador)[0], :]
 
+# cambiar nombre de 'datetime' a uno estandar que uso 'timestamp'
+df_ice = df_ice.rename(columns={'DateTime': 'timestamp'})
+
+# cambiar a minusculas los nombres de las columnas
+columnas = list(df_ice.columns)
+columnas = [i.lower() for i in columnas]
+df_ice.rename(columns=dict(zip(df_ice.columns[1:], columnas)), inplace=True)
+
 # resetear index en dataframe resultante porque guarda los indices del dataframe pasado
 df_ice = df_ice.reset_index(drop=True)
+
+# convertir columna a datetime
+df_ice['DateTime'] = pd.to_datetime(df_ice['DateTime'])
 
 # -- criterio para rellenar datos faltantes 'nan'
 # cuando falta en consensus
@@ -65,41 +78,138 @@ df_ice['escenario'][((df_ice['Actual'] < df_ice['Previous']) & (df_ice['Actual']
 df_ice['escenario'][((df_ice['Actual'] < df_ice['Previous']) & (df_ice['Actual'] < df_ice['Consensus']) &
                     (df_ice['Consensus'] < df_ice['Previous']))] = 'H'
 
+# renombrar las columnas
+df_ice = df_ice.rename(columns={'DateTime': 'TimesTamp'})
+
 # -- ------------------------------------------------------------------------------------ Datos: Precios con OANDA -- #
 # -- --------------------------------------------------------------------------------------- --------------------- -- #
 
-A1_OA_Da = 16                      # Day Align
-A1_OA_Ai = "101-004-2221697-001"   # Id de cuenta
-A1_OA_At = "practice"              # Tipo de cuenta
-A1_OA_In = "EUR_USD"               # Instrumento
-A1_OA_Gn = "M5"                    # Granularidad de velas
-FechaIni = "2009-01-02T00:00:00Z"  # Fecha inicial
-FechaFin = "2019-09-30T00:00:00Z"  # Fecha final
+OA_Da = 16                         # Day Align
+OA_Ai = "101-004-2221697-001"      # Id de cuenta
+OA_At = "practice"                 # Tipo de cuenta
+OA_In = "EUR_USD"                  # Instrumento
+OA_Gn = "M5"                        # Granularidad de velas
+FechaIni = "2009-01-06 00:00:00"   # Fecha inicial
+FechaFin = "2019-09-27 00:00:00"   # Fecha final
+
+# cohercionar las fechas a huso horario GMT
+fini = pd.to_datetime(FechaIni).tz_localize('GMT')
+ffin = pd.to_datetime(FechaFin).tz_localize('GMT')
 
 # Token para API de OANDA
-A1_OA_Ak = '7' + '9ae0a52f8e483facdd81f5b316a8ef8-99fb5554f4739c76535b209044f7de2' + '6'
+OA_Ak = '7' + '9ae0a52f8e483facdd81f5b316a8ef8-99fb5554f4739c76535b209044f7de2' + '6'
 
-# -- ------------------------------------------------------------------------------------ Inicializar API de OANDA -- #
 
-api = API(access_token=A1_OA_Ak)
+# -- --------------------------------------------------------------------------------------------- rango de fechas -- #
+
+def f_datetime_range_fx(start_datetime, end_datetime, increment, delta_period):
+    """
+    :param start_datetime:
+    :param end_datetime:
+    :param increment:
+    :param delta_period:
+
+    :return:
+    """
+
+    result = []
+    nxt = start_datetime
+
+    while nxt <= end_datetime:
+        result.append(nxt)
+        if delta_period == 'minutes':
+            nxt += timedelta(minutes=increment)
+        elif delta_period == 'hours':
+            nxt += timedelta(hours=increment)
+        elif delta_period == 'days':
+            nxt += timedelta(days=increment)
+
+    return result
+
 
 # -- ---------------------------------------------------------------------------------- Obtener precios historicos -- #
 
-params = {"granularity": A1_OA_Gn, "price": "M", "dailyAlignment": A1_OA_Da, "from": FechaIni, "to": FechaFin}
+def f_precios_masivos(p0_fini, p1_ffin, p2_gran, p3_inst, p4_oatk, p5_ginc):
+    """
+    :param p0_fini: fecha inicial
+    :param p1_ffin: fecha final
+    :param p2_gran: granularidad de los precios
+    :param p3_inst: instrumento
+    :param p4_oatk: llave privada de oanda para peticiones con api
+    :param p5_ginc: incremento de valor para fechas dinamicas
 
-A1_Req1 = instruments.InstrumentsCandles(instrument=A1_OA_In, params=params)
-A1_Hist = api.request(A1_Req1)
+    :return: final: dataframe final con todos los precios historicos
 
-lista = []
-for i in range(len(A1_Hist['candles'])-1):
-    lista.append({'TimeStamp': A1_Hist['candles'][i]['time'],
-                  'Open': A1_Hist['candles'][i]['mid']['o'],
-                  'High': A1_Hist['candles'][i]['mid']['h'],
-                  'Low': A1_Hist['candles'][i]['mid']['l'],
-                  'Close': A1_Hist['candles'][i]['mid']['c']})
+    p0_fini = fini
+    p1_ffin = ffin
+    p2_gran = OA_Gn
+    p3_inst = OA_In
+    p4_oatk = OA_Ak
+    p5_ginc = gn[OA_Gn]['in']
 
-# -- ------------------------------------------------------------------------------------------------ Salida final -- #
+    """
 
-df_hist = pd.DataFrame(lista)
-df_hist['TimeStamp'] = pd.to_datetime(df_hist['TimeStamp'])
-df_hist = df_hist[['TimeStamp', 'Open', 'High', 'Low', 'Close']]
+    # inicializar api de OANDA
+    api = API(access_token=p4_oatk)
+
+    # hacer series de fechas e iteraciones para pedir todos los precios
+    fechas = f_datetime_range_fx(start_datetime=p0_fini, end_datetime=p1_ffin, increment=p5_ginc,
+                                 delta_period='minutes')
+
+    # Lista para ir guardando los data frames
+    lista_df = list()
+
+    for n_fecha in range(0, len(fechas)-1):
+
+        # Fecha inicial y fecha final
+        f1 = fechas[n_fecha].strftime('%Y-%m-%dT%H:%M:%S')
+        f2 = fechas[n_fecha+1].strftime('%Y-%m-%dT%H:%M:%S')
+
+        # Parametros pra la peticion de precios
+        params = {"granularity": p2_gran, "price": "M", "dailyAlignment": OA_Da, "from": f1, "to": f2}
+
+        # Ejecutar la peticion de precios
+        a1_req1 = instruments.InstrumentsCandles(instrument=p3_inst, params=params)
+        a1_hist = api.request(a1_req1)
+
+        # Para debuging
+        # print(f1 + ' y ' + f2)
+        lista = list()
+
+        # Acomodar las llaves
+        for i in range(len(a1_hist['candles']) - 1):
+            lista.append({'TimeStamp': a1_hist['candles'][i]['time'],
+                          'Open': a1_hist['candles'][i]['mid']['o'],
+                          'High': a1_hist['candles'][i]['mid']['h'],
+                          'Low': a1_hist['candles'][i]['mid']['l'],
+                          'Close': a1_hist['candles'][i]['mid']['c']})
+
+        # Acomodar en un data frame
+        pd_hist = pd.DataFrame(lista)
+        pd_hist = pd_hist[['TimeStamp', 'Open', 'High', 'Low', 'Close']]
+        pd_hist['TimeStamp'] = pd.to_datetime(pd_hist['TimeStamp'])
+
+        # Ir guardando resultados en una lista
+        lista_df.append(pd_hist)
+
+    # Concatenar todas las listas
+    final = pd.concat([lista_df[i] for i in range(0, len(lista_df))])
+
+    return final
+
+
+# -- -------------------------------------------------------------------------------- Proceso de descarga completo -- #
+# -- -------------------------------------------------------------------------------- ---------------------------- -- #
+# -- Solo correr esta parte si se quiere descargar todos los precios
+
+# Descagar todos los precios necesarios
+# df_precios = f_precios_masivos(p0_fini=fini, p1_ffin=ffin, p2_gran=OA_Gn, p3_inst=OA_In, p4_oatk=OA_Ak, p5_ginc=5000)
+
+# Resetear index
+# df_precios = df_precios.reset_index(drop=True)
+
+# Escribir dataframe en un archivo csv
+# df_precios.to_csv(r"archivos/precios_historicos.csv")
+
+# Leer archivo de precios historicos
+df_precios = pd.read_csv("archivos/precios_historicos.csv")
--- anova/modelo.py
+++ anova/modelo.py
@@ -6,7 +6,7 @@
 # -- ------------------------------------------------------------------------------------------------------------- -- #
 
 import pandas as pd
-from datos import df_ice, df_hist
+from datos import df_ice, df_precios
 
 pd.set_option('display.max_rows', None)                   # sin limite de renglones maximos para mostrar pandas
 pd.set_option('display.max_columns', None)                # sin limite de columnas maximas para mostrar pandas
@@ -14,5 +14,23 @@ pd.set_option('display.width', None)                      # sin limite el ancho 
 pd.set_option('display.expand_frame_repr', False)         # visualizar todas las columnas de un pandas dataframe
 pd.options.mode.chained_assignment = None                 # para evitar el warning enfadoso de indexacion
 
-print(df_hist)
-print(df_ice)
+
+# -- ---------------------------------------------------------------------------------------- definir metrica base -- #
+# -- ---------------------------------------------------------------------------------------- -------------------- -- #
+
+# -- Ambas fechas, tanto del calendario economico como de los precios estan en Huso horario GMT
+
+# se define como tiempo inicial como aquel en el que se comunico el indicador
+
+# se define como precio inicial el precio de apertura en el tiempo inicial
+
+# para cada evento del indicador se hace una ventana de T tiempo con todos los precios OHLC
+
+# hacer el calculo de (high - precio inicial) y (precio inicial - low) con los precios de la ventana de T tiempo
+# hacer calculo de amplitud de movimiento entre precio inicial y high y low durante la ventana
+
+# High - Open  (a 10 minutos despues)
+# Open - High  (a 10 minutos despues)
+
+# print(df_precios)
+# print(df_ice)
