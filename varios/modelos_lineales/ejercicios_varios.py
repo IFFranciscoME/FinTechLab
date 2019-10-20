e3f67a39675d6d64a6279caa3314d1a1963cc1d2
IFFranciscoME <if.francisco.me@gmail.com>
2019-10-20 10:04:13 -0500

    varios ejercicios

---
+++ varios/modelos_lineales/ejercicios_varios.py
@@ -0,0 +1,9 @@
+
+# -- ------------------------------------------------------------------------------------------------------------- -- #
+# -- Proyecto: varios ejercicios sobre aplicaciones de modelos lineales
+# -- Codigo: ejercicios_varios.py
+# -- Autor: Francisco ME
+# -- Repositorio: https://github.com/IFFranciscoME/FinTechLab/blob/master/
+# -- ------------------------------------------------------------------------------------------------------------- -- #
+
+
