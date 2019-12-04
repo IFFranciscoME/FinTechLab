57a693e1278cb452f49fd44aa2342ce1e7074a89
IFFranciscoME <if.francisco.me@gmail.com>
2019-12-04 14:22:33 -0600

    primera version de RLM

--- MCD-ITESO/AEM/aem_proyecto/funciones.py
+++ MCD-ITESO/AEM/aem_proyecto/funciones.py
@@ -13,6 +13,9 @@ from statsmodels.tsa.api import acf, pacf
 from oandapyV20 import API
 import oandapyV20.endpoints.instruments as instruments
 
+import statsmodels.api as sm                              # utilidades para modelo regresion lineal
+from sklearn.model_selection import train_test_split      # separacion de conjunto de entrenamiento y prueba
+
 pd.set_option('display.max_rows', None)                   # sin limite de renglones maximos para mostrar pandas
 pd.set_option('display.max_columns', None)                # sin limite de columnas maximas para mostrar pandas
 pd.set_option('display.width', None)                      # sin limite el ancho del display
@@ -100,9 +103,11 @@ def f_feature_eng(p_datos, p_ohlc):
 
     # rendimiento logaritmico de ventana 1
     datos['logrend'] = np.log(datos['close']/datos['close'].shift(1)).dropna()
+    # signo del rendimiento para indicar "alcista = 1" o "bajista = 0"
+    datos['logrend_c'] = [1 if datos['logrend'][i] > 0 else -1 for i in range(0, len(datos['logrend']))]
 
     # diferencia de high - low como medida de "volatilidad"
-    datos['hl'] = datos['high'] - datos['low']
+    datos['hl'] = (datos['high'] - datos['low'])*10000
 
     # funciones de ACF y PACF para determinar ancho de ventana historica
     data_acf = acf(datos['logrend'].dropna(), nlags=52, fft=True)
@@ -125,9 +130,6 @@ def f_feature_eng(p_datos, p_ohlc):
         # promedio movil de ventana n con log rendimiento
         datos['ma_logrend_' + str(n+2)] = datos['logrend'].rolling(n+2).mean()
 
-        # log rendimiento de ventana n
-        datos['logrend_' + str(n+1)] = np.log(datos['close']/datos['close'].shift(n+1))
-
         # resago n de high - low
         datos['lag_hl_' + str(n+1)] = datos['hl'].shift(n+1)
 
@@ -137,6 +139,10 @@ def f_feature_eng(p_datos, p_ohlc):
         # promedio movil de ventana n con high - low
         datos['ma_hl_' + str(n+2)] = datos['hl'].rolling(n+2).mean()
 
+    # asignar timestamp como index
+    datos.index = datos['timestamp']
+    # quitar columnas no necesarias para modelos de ML
+    datos = datos.drop(['timestamp', 'open', 'high', 'low', 'close', 'hl'], axis=1)
     # borrar columnas donde exista solo NAs
     r_features = datos.dropna(axis='columns', how='all')
     # borrar renglones donde exista algun NA
@@ -147,14 +153,88 @@ def f_feature_eng(p_datos, p_ohlc):
     return r_features
 
 
-# -- ------------------------------------------------------------------------------ FUNCION: Analisis de variables -- #
+# -- ----------------------------------------------------------------------------- FUNCION: Seleccion de variables -- #
 # -- ------------------------------------------------------------------------------ ------------------------------ -- #
 
-def f_feature_an(p_datos):
+def f_feature_importance(p_datos):
     """
     :param p_datos:
     :return:
+    p_datos = df_datos
     """
+
+    # np.corrcoef()
 
     return 1
 
+
+# -- -------------------------------------------------------------------------------- FUNCION: Ajustar RLM a datos -- #
+# -- -------------------------------------------------------------------------------- ---------------------------- -- #
+
+def f_rlm(p_datos):
+    """
+    :param p_datos: pd.DataFrame : DataFrame con variable "y" (1era col), y n variables "x_n" (2:n)
+    :return:
+    p_datos = df_datos
+    """
+
+    datos = p_datos
+
+    # Reacomodar los datos como arreglos
+    y_multiple = np.array(datos.iloc[:, 1])
+    x_multiple = np.array(datos.iloc[:, 2:])
+
+    # datos para entrenamiento y prueba
+    train_x, test_x, train_y, test_y = train_test_split(x_multiple, y_multiple, test_size=0.8, shuffle=False)
+
+    # Agregar interceptos a X en entrenamiento y prueba
+    train_x_betha = sm.add_constant(train_x)
+    test_x_betha = sm.add_constant(test_x)
+
+    # Modelo ajustado (entrenamiento)
+    modelo_train = sm.OLS(train_y, train_x_betha)
+    # Resultados de ajuste de modelo (entrenamiento)
+    modelo_fit_train = modelo_train.fit()
+
+    # Modelo ajustado (prueba)
+    modelo_test = sm.OLS(test_y, test_x_betha)
+    # Resultados de ajuste de modelo (prueba)
+    modelo_fit_test = modelo_test.fit()
+
+    # -- Con datos de ENTRENAMIENTO
+    # modelo completo resultante
+    r_train_modelo = modelo_fit_train
+    # summary de resultados del modelo
+    r_train_summary = r_train_modelo.summary()
+    # DataFrame con nombre de parametros segun dataset, nombre de parametros y pvalues segun modelo
+    r_df_train = pd.DataFrame({'df_params': ['intercepto'] + list(datos.columns[2:]),
+                               'm_params': r_train_modelo.model.data.param_names, 'pv_params': r_train_modelo.pvalues})
+    # valor de AIC del modelo
+    r_train_aic = r_train_modelo.aic
+    # valor de BIC del modelo
+    r_train_bic = r_train_modelo.bic
+
+    # -- Con datos de PRUEBA
+    # modelo completo resultante
+    r_test_modelo = modelo_fit_test
+    # summary de resultados del modelo
+    r_test_summary = r_test_modelo.summary()
+    # DataFrame con nombre de parametros segun dataset, nombre de parametros y pvalues segun modelo
+    r_df_test = pd.DataFrame({'df_params': ['intercepto'] + list(datos.columns[2:]),
+                              'm_params': r_test_modelo.model.data.param_names, 'pv_params': r_test_modelo.pvalues})
+    # valor de AIC del modelo
+    r_test_aic = r_test_modelo.aic
+    # valor de BIC del modelo
+    r_test_bic = r_test_modelo.bic
+
+    # tabla de resultados periodo de entrenamiento
+    r_df_pred_train = pd.DataFrame({'y': train_y, 'y_ajustada': modelo_fit_train.predict()})
+    # tabla de resultados periodo de prueba
+    r_df_pred_test = pd.DataFrame({'y': test_y, 'y_ajustada': modelo_fit_test.predict()})
+
+    r_d_modelo = {'train': {'modelo': r_train_modelo, 'summary': r_train_summary, 'parametros': r_df_train,
+                            'resultado': r_df_pred_train, 'aic': r_train_aic, 'bic': r_train_bic},
+                  'test': {'modelo': r_test_modelo, 'summary': r_test_summary, 'parametros': r_df_test,
+                            'resultado': r_df_pred_test, 'aic': r_test_aic, 'bic': r_test_bic}}
+
+    return r_d_modelo
--- MCD-ITESO/AEM/aem_proyecto/principal.py
+++ MCD-ITESO/AEM/aem_proyecto/principal.py
@@ -9,10 +9,13 @@ import funciones as fn
 
 # -- datos de entrada
 df_precios = fn.f_precios(p_fuente='oanda', p_ins='EUR_USD', p_grn='W',
-                          p_fini='2013-01-01T00:00:00Z', p_ffin='2019-11-29T00:00:00Z')
+                          p_fini='2009-01-01T00:00:00Z', p_ffin='2019-10-31T00:00:00Z')
+
 
 # -- generacion de variables endogenas
 df_datos = fn.f_feature_eng(p_datos=df_precios, p_ohlc=True)
 
 # -- analisis de variables
 
+# -- ajute de modelo 1: RLM con variables endogenas (Sin tratamiento)
+resultado = fn.f_rlm(df_datos)
